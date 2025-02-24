1b82c743bbd7e08b0348977eccef8b9e10280717
Shandrae Lois Quianzon <saquianzon@student.apc.edu.ph>
2025-02-24 06:55:46 +0000

    upd

--- my_flask_app/app.py
+++ my_flask_app/app.py
@@ -5,6 +5,7 @@ app = Flask(__name__)
 books = [
     {"id": 1, "title": "1984", "author": "George Orwell"},
     {"id": 2, "title": "To Kill a Mockingbird", "author": "Harper Lee"}
+]
 
 @app.route('/books', methods=['GET'])
 def get_books():
